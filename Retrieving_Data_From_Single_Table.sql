916d09dde877bed358dceb56e788b7afab179b16
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-14 14:33:16 -0500

    Update Retrieving_Data_From_Single_Table.sql

--- Retrieving_Data_From_Single_Table.sql
+++ Retrieving_Data_From_Single_Table.sql
@@ -5,3 +5,9 @@ SELECT * FROM customers;
 SELECT * FROM customers WHERE customer_id = 1;
 
 SELECT * FROM customers order by birth_date;
+
+SELECT last_name, first_name, points FROM customers order by points;
+
+SELECT 
+last_name, first_name, points, (points + 10) * 100 AS discount_factor
+FROM customers order by points;
